e6b9b7e5d65cbaeb4d9185ba5016d6b73c06f478
Marcos Oscar Frigerio <marcos.frigerio@intive.com>
2022-10-04 16:41:31 -0300

    Envio de un token al servidor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { connectToServer } from './socket-client'
-import './style.css'
+import { connectToServer } from "./socket-client";
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+// El jwt se va a usar para verificar el usuario
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <h1>Websocket - Client </h1>
-    // <span id="server-status">offline</span>
+    <h2>Websocket - Client </h2>
+    <input id="jwt-token" placeholder="Jason web token" />
+    <button id="btn-connect">Conect</button>
+    <br/>
+    <span id="server-status">offline</span>
 
     <ul id="client-ul"></ul>
 
@@ -15,8 +19,17 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h3>Messages</h3>
     <ul id="message-ul"></ul>
   </div>
-`
+`;
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 // con esta conexion, se puede ver en el servidor, los mensajes de conexion y desconexion en la consola
-connectToServer();
+// connectToServer(); // Aca se conecta directamente al abrir el sitio.
+
+const btnConnect = document.querySelector<HTMLButtonElement>("#btn-connect");
+const jwtToken = document.querySelector<HTMLInputElement>("#jwt-token");
+
+btnConnect?.addEventListener("click", () => {
+  if (!jwtToken || jwtToken.value.trim().length <= 0) return alert("Enter a valid JWT"); // valida que haya un token
+
+  connectToServer(jwtToken.value.trim()); // envia el token al servidor
+});
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,9 +3,14 @@ import { Manager, Socket } from "socket.io-client"
 
 
 // el socket.io-client (verificar que la version de la url coincida con la instalada)
-export const connectToServer = () => {
+export const connectToServer = (token: string) => {
   // 
-  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js') // uri donde se puede encontrar un socket server (correspondiente a teslo-shop)
+  const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+    extraHeaders: {
+      hola: 'mundo',
+      authentication: token,
+    },
+  }) // uri donde se puede encontrar un socket server (correspondiente a teslo-shop)
 
   // conexion con el namespace "root(/)" de la aplicacion
   // La conexion mediante un socket es cliente-servidor. no se puede hablar directamente con otro cliente
